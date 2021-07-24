77b80f247f967502de1a6b374d372479f2d0d5e9
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2021-07-24 15:48:28 -0500

    Add pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // TODO: have buttons to start new 1P or 2P
+    public static bool isPaused = true;
+    protected static bool gameOngoing = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && gameOngoing)
+        {
+            if (isPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        gameOngoing = true;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Quit()
+    {
+        print("Quitting game...");
+        Application.Quit();
+    }
+}
